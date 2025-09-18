5d4ec44d7be4b880da985f347c033c5d1eb736d9
namedummy568-stack <namedummy568@gmail.com>
2025-09-18 17:34:53 +0530

    Refine type deduction for nested templates: Use const references

--- src/type_deduction.cpp
+++ src/type_deduction.cpp
@@ -1,1 +1,1 @@
-template<typename T> struct Outer { template<typename U> struct Inner { auto get_val(T t, U u) { return t + u; } }; };
+template<typename T> struct Outer { template<typename U> struct Inner { auto get_val(const T& t, const U& u) { return t + u; } }; };
